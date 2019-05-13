794d6fe8281ccaf096dd771edf7198a8a9e24e8b
nalm <nalm@users.noreply.github.com>
2019-05-13 20:54:07 +0100

    design
    melhoradas as interações com o utilizador
     + efeito de aproximação no produto

--- JS/carregarProdutos.js
+++ JS/carregarProdutos.js
@@ -11,7 +11,7 @@ function carregar_produtos(destino){
             div_destino.innerHTML = "<tr>"
             $.each(data.result, function (row, produto){
                 produtos.push(produto);
-                html_row += "<td><div class=\"imagem_descontos\"><img src=\""+ produto.imagem +"\" alt=\""+produto.id+"\"><div class=\"label_imagem_descontos\">"+produto.nome +" "+produto.preco+"€</div></div></td>";
+                html_row += "<td><div class=\"imagem_descontos\"><img src=\""+ produto.imagem +"\" alt=\""+produto.id+"\"><div class=\"label_imagem_descontos\">"+produto.nome +"</div><div class=\"preco_imagem_descontos\">"+produto.nome+" "+produto.preco+"€</div></div></td>";
                 i+=1;
                 if(i > 3) {
                     html_row += "</tr>";
